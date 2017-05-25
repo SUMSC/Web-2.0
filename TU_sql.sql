13c38621ceccca48da0982b1e33ada7ee8cedb32
creating-t <1789912638@qq.com>
2017-05-25 20:54:40 +0800

    Update TU_sql.sql

--- TU_sql.sql
+++ TU_sql.sql
@@ -6,14 +6,14 @@ use INFORMATION;
 
 create table files(
 
-	    uname varchar(32),  -- 用户名
-	    admin varchar(4),  -- 用户对该文件的管理权限
+	    username varchar(32),  -- 用户名
+	    administration varchar(4),  -- 用户对该文件的管理权限
 	    filename varchar(50),
-	    filedir varchar(50),  -- 位置
+	    filedirectory varchar(50),  -- 位置
 	    uploadtime DATETIME,  -- 上传时间
 	    changetime DATETIME,   -- 更改时间
 	    fileid int auto_increment,
-	    ftype varchar(5),   -- 文件类型(格式)
+	    filetype varchar(5),   -- 文件类型(格式)
 	    remarks varchar(100),   -- 标记备注啥的
 
         primary key(fileid)

8340b3afda96bdd7ad0f35446e9b6d6bc762890b
FayeTitus <2359981832@qq.com>
2017-05-25 20:53:10 +0800

    PF_sql comes!

---
+++ PF_sql.sql
@@ -0,0 +1,30 @@
+drop database if exists `Web`;
+
+create database `Web` default character set utf8 COLLATE utf8_general_ci ;
+
+use `Web`;
+
+create table repairmen(
+
+	rid int unsigned auto_increment,
+	name varchar(32),
+	contact varchar(64),
+	gender varchar(16),
+	free int default '0',
+
+	primary key(rid)
+    
+)engine=InnoDB default charset=utf8;
+
+create table consult(
+
+	cid int unsigned auto_increment,
+	rid int NOT NULL, 
+	consultant varchar(16),
+	consultantContact varchar(24),    
+	description varchar(1024),
+	submitTime datetime,
+	primary key(cid)
+	
+)engine=InnoDB auto_increment=1 default charset=utf8;
+

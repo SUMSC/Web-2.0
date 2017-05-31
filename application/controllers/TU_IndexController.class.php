b864e7868b2dbce2e1e1603118f15ec4b81cb9c5
creating-t <1789912638@qq.com>
2017-05-31 12:46:30 +0800

    Update TU_IndexController.class.php

--- application/controllers/TU_IndexController.class.php
+++ application/controllers/TU_IndexController.class.php
@@ -1,17 +1,12 @@
 <?php 
 
-class UploadController extends Controller{
+class TU_IndexController extends Controller{
 
     function index(){     //当url为 /upload 或 /upload/index时执行函数
 
         $this->assign('title', '首页');
         $this->assign('content', 'php MVC');
-        $this->render("upload");
-    }
-
-    public function upload()               //为 /upload/upload 时执行
-    {
-        $this->render("upload");
+        $this->render("tu_index");
     }
 }
 

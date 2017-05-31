cf35cb4eed0ed952caa94193dc6f4a5054884301
Paladnix <Paladnix@outlook.com>
2017-05-31 20:54:00 +0800

    wu

--- application/controllers/GT_IndexController.class.php
+++ application/controllers/GT_IndexController.class.php
@@ -6,7 +6,9 @@ class GT_IndexController extends Controller{
     public function index()
     {
     	$data = (new GT_IndexModel)->select("gtBook");
-    	
+        // book.php 调用方式 $vars["data"];
+        $this->assign("data", $data);
+
         $this->render("index");
     }
 }
--- application/views/GT_Index/book.php
+++ application/views/GT_Index/book.php
@@ -2,6 +2,10 @@
 <div class="list-group">
 
 <?php
+// 使用assign()设定的数据应该用 $vars['name'] 的方式来调用。
+?>
+
+<?php
 $data  = array("book1"=>"www.baidu.com","book2"=>"www.sina.com");
 foreach($data as $k => $v)
 {

bb8432a138b38fd433028f302afffbd9248453e0
creating-t <1789912638@qq.com>
2017-06-02 19:03:44 +0800

    Update TU_IndexView.class.php

--- application/views/TU_IndexView.class.php
+++ application/views/TU_IndexView.class.php
@@ -2,10 +2,30 @@
 
 class TU_IndexView extends View{
 
-    public function tu_index(){
-
-        render("tu_index");
-    
+    public function render($action="index"){
+        if($action == "index")
+        {
+            $this->index(); return ;
+        }
+        else if($action == "test")
+        {
+	        $this->test(); return ;
+        }
+        else
+        {
+            $this->index(); return ;
+        }
     }
-
+    
+    public function index(){
+        echo "this is index";
+    }
+    
+    public function test(){
+		$pages[] = APP_PATH."application/views/TU_Index/test.php";
+		$pages[] = APP_PATH."application/views/TU_Index/link.php";
+        foreach($pages as $page){
+			require($page);
+		}
+    }
 }

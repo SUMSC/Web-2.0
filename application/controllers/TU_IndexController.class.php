62bda49f0969ff69e1bd3629995d9731ad61ccf0
creating-t <1789912638@qq.com>
2017-06-02 19:05:32 +0800

    Update TU_IndexController.class.php

--- application/controllers/TU_IndexController.class.php
+++ application/controllers/TU_IndexController.class.php
@@ -2,11 +2,17 @@
 
 class TU_IndexController extends Controller{
 
-    function index(){     //当url为 /upload 或 /upload/index时执行函数
+    function index(){     
 
         $this->assign('title', '首页');
         $this->assign('content', 'php MVC');
         $this->render("tu_index");
     }
+    
+    public function test(){
+    
+        $this->render("test");
+    }
+
 }
 

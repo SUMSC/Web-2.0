cd4fd6ae0abbb6784ed2e5fe12db27b9cb8cc568
FayeTitus <2359981832@qq.com>
2017-08-03 15:06:19 +0800

    ..

---
+++ application/views/Index/content.php
@@ -0,0 +1,47 @@
+<div class="jumbotron">
+	<div class="container-fluid">
+		<h1>Hello!This is SUMSC!</h1>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-2"></div>
+				<div class="col-md-10">
+					<p>这是一个简单的小站,仅仅用于学生间的学习交流,在这里,您可以:	</p>
+				</div>
+			</div>
+		</div>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-4"></div>
+				<div class="col-md-8">
+					<li class="text-primary">联系我们的志愿者，获得一些电脑问题的技术协助。</li>
+				</div>
+			</div>
+		</div>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-4"></div>
+				<div class="col-md-8">
+					<li class="text-primary">转至我们甄选出的一些外部网站，它们往往实用却鲜为人知。</li>
+				</div>
+			</div>
+		</div>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-4"></div>
+				<div class="col-md-8">
+					<li class="text-primary">也许还有一些有趣的技术博文。</li>
+				</div>
+			</div>
+		</div>
+		<br><br><br><br><br>
+
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-2"></div>
+				<div class="col-md-10">
+					<p class="text-muted">我们会逐渐挂上一些好玩的小东西，直到下一版的官网出现。</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>

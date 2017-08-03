49d96a06030093fa134d5f90e2848988d39fbe02
FayeTitus <2359981832@qq.com>
2017-08-03 10:38:58 +0800

    charset=utf-8

--- application/views/TU_Index/index.php
+++ application/views/TU_Index/index.php
@@ -1,7 +1,10 @@
 <?php
   //上传框，查看、搜索接口
 ?>
-  
+<head>
+  <meta charset="utf-8">
+</head>
+<body>
   <form action="index.php" method="post" enctype="multipart/form-data" class="t_upform_static">
     <p><label for="t_file" id="t_fileLabel"><span>点我上传文件</span></label></p>
     <input type="file" name="file" hidden="hidden" id="t_file" />
@@ -9,3 +12,4 @@
     <span class="t_uploadtips">只有<a href="javascript:void(0);">登录</a>用户可以上传大文件(10M)哦</span>
     <p><input type="submit" value="提交" class="t_btn" id="t_fsubmit" disabled="disabled"/></p>
   </form>
+</body>
